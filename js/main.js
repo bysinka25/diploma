8a33983eae75cb4a2bd141a6a8bb2ba95ab24bdf
bysinka25 <kirgizilla@gmail.com>
2021-06-04 01:01:37 +0300

    fixed

--- js/main.js
+++ js/main.js
@@ -12,4 +12,32 @@ const swiper = new Swiper(".swiper-container", {
     enabled: "true",
     onlyInViewport: "true",
   },
+  breakpoints: {
+    // when window width is <= 499px
+    576: {
+      slidesPerView: 1,
+      spaceBetweenSlides: 30,
+    },
+    // when window width is <= 999px
+    1920: {
+      slidesPerView: 4,
+      spaceBetweenSlides: 40,
+    },
+    1440: {
+      slidesPerView: 4,
+      spaceBetweenSlides: 40,
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetweenSlides: 20,
+    },
+    991: {
+      slidesPerView: 3,
+      spaceBetweenSlides: 40,
+    },
+    776: {
+      slidesPerView: 2,
+      spaceBetweenSlides: 40,
+    },
+  },
 });
