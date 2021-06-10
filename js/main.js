bcfabc423fd3e0d3afad781ffea73682b4c6daec
bysinka25 <kirgizilla@gmail.com>
2021-06-10 19:06:38 +0300

    create like

--- js/main.js
+++ js/main.js
@@ -24,9 +24,15 @@ const swiper = new Swiper(".merit-container", {
     },
     1200: {
       slidesPerView: 3,
+      slidesPerColumn: 1,
     },
     1440: {
       slidesPerView: 4,
+      slidesPerColumn: 1,
+    },
+    1920: {
+      slidesPerView: 2,
+      slidesPerColumn: 1,
     },
   },
 });
@@ -144,7 +150,7 @@ $(document).keyup(function (event) {
 });
 var menuButton = document.querySelector(".menu-button");
 menuButton.addEventListener("click", function () {
-  console.log("Клик по кнопке меню");
+  // console.log("Клик по кнопке меню");
   document.querySelector(".header-nav").classList.toggle("header-nav--visible");
   var $body = $(document.body);
   if ($body.css("overflow") == "hidden") {
@@ -181,3 +187,162 @@ $(".newsletter__form").validate({
   },
 });
 $(".modal__input--phone_us").mask("+7 (000) 000-00-00");
+// $(window).scroll(function () {
+//   var height = $(window).scrollTop();
+//   if (height > 100) {
+//     $("#top").fadeIn();
+//   } else {
+//     $("#top").fadeOut();
+//   }
+// });
+// $(document).ready(function () {
+//   $("#top").click(function (event) {
+//     event.preventDefault();
+//     $("html, body").animate({ scrollTop: 0 }, "slow");
+//     return false;
+//   });
+// });
+$("a[href='#top']").click(function () {
+  $("html, body").animate({ scrollTop: 0 }, "slow");
+  return false;
+});
+// $("a[href='#book']").click(function () {
+//   $("html, body").animate({ scrollTop: 0 }, "slow");
+//   return false;
+// });
+// $("a[href='#blog']").click(function () {
+//   $("html, body").animate({ scrollBottom: 0 }, "slow");
+//   return false;
+// });
+// var basicScrollTop = function () {
+//   // The button
+//   var btnTop = document.querySelector(".anchor__button");
+//   // Reveal the button
+//   var btnReveal = function () {
+//     var position = document.querySelector("#position");
+//     position.innerHTML = window.scrollY + "px";
+//     if (window.scrollY >= 300) {
+//       btnTop.classList.add("__is-visible");
+//     } else {
+//       btnTop.classList.remove("__is-visible");
+//     }
+//   };
+//   // Smooth scroll top
+//   // Thanks to => http://stackoverflow.com/a/22610562
+//   var TopscrollTo = function () {
+//     if (window.scrollY != 0) {
+//       setTimeout(function () {
+//         window.scrollTo(0, window.scrollY - 30);
+//         TopscrollTo();
+//       }, 5);
+//     }
+//   };
+//   // Listeners
+//   window.addEventListener("scroll", btnReveal);
+//   btnTop.addEventListener("click", TopscrollTo);
+// };
+// basicScrollTop();
+// var btn = $(".anchor__button");
+
+// $(window).scroll(function () {
+//   if ($(window).scrollTop() > 300) {
+//     btn.addClass(".anchor");
+//   } else {
+//     btn.removeClass(".anchor");
+//   }
+// });
+
+// btn.on("click", function (e) {
+//   e.preventDefault();
+//   $("html, body").animate({ scrollTop: 0 }, "300");
+// });
+document.addEventListener("DOMContentLoaded", function (event) {
+  // 选择按钮
+  var back2top = document.getElementsByClassName("anchor__button")[0];
+
+  // 滚动到一定位置显示按钮
+  document.addEventListener("scroll", function (event) {
+    if (getScollTop() > 1500) {
+      back2top.style.display = "block";
+    } else {
+      back2top.style.display = "none";
+    }
+  });
+
+  // 点击按钮返回顶部
+  back2top.addEventListener("click", function (event) {
+    window.scrollTo(0, 0);
+  });
+});
+
+// 获取滚动位置函数
+function getScollTop() {
+  var scrollTop = 0;
+  if (window.pageYOffset) {
+    scrollTop = window.pageYOffset;
+  } else if (document.documentElement && document.documentElement.scrollTop) {
+    scrollTop = document.documentElement.scrollTop;
+  } else if (document.body) {
+    scrollTop = document.body.scrollTop;
+  }
+  return scrollTop;
+}
+$(document).ready(function () {
+  $("#blog").on("click", "a", function (event) {
+    //отменяем стандартную обработку нажатия по ссылке
+
+    event.preventDefault();
+
+    //забираем идентификатор бока с атрибута href
+
+    var id = $(this).attr("href"),
+      //узнаем высоту от начала страницы до блока на который ссылается якорь
+
+      top = $(id).offset().top;
+
+    //анимируем переход на расстояние - top за 1500 мс
+
+    $("body,html").animate({ scrollTop: top }, 1500);
+  });
+});
+$(document).ready(function () {
+  $("#book").on("click", "a", function (event) {
+    //отменяем стандартную обработку нажатия по ссылке
+
+    event.preventDefault();
+
+    //забираем идентификатор бока с атрибута href
+
+    var id = $(this).attr("href"),
+      //узнаем высоту от начала страницы до блока на который ссылается якорь
+
+      top = $(id).offset().top;
+
+    //анимируем переход на расстояние - top за 1500 мс
+
+    $("body,html").animate({ scrollTop: top }, 1500);
+  });
+});
+$(document).ready(function () {
+  $("#best").on("click", "a", function (event) {
+    //отменяем стандартную обработку нажатия по ссылке
+
+    event.preventDefault();
+
+    //забираем идентификатор бока с атрибута href
+
+    var id = $(this).attr("href"),
+      //узнаем высоту от начала страницы до блока на который ссылается якорь
+
+      top = $(id).offset().top;
+
+    //анимируем переход на расстояние - top за 1500 мс
+
+    $("body,html").animate({ scrollTop: top }, 1500);
+  });
+});
+document.querySelectorAll(".like").forEach((like) => {
+  like.addEventListener("click", () => {
+    like.classList.toggle("active");
+  });
+});
